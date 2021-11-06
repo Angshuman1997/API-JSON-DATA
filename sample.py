270fbd2c0dd18d92c5aeb1af2e63ec4389565452
Angshuman1997 <bardhanangshu@gmail.com>
2021-11-06 10:29:03 +0530

    File updated

--- sample.py
+++ sample.py
@@ -3,10 +3,10 @@ from flask import Flask, request
 
 app= Flask(__name__)
 
-@app.route('/hello',methods=['GET'])
+@app.route('/',methods=['GET'])
 def hello():
     if request.method=='GET':
         return('hello')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
